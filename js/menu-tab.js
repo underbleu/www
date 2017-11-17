ac9bdb471606026bda9d8efd073c446097c86de9
underbleu <underbleu.dev@gmail.com>
2017-11-17 11:57:12 +0900

    메뉴탭 jQuery완성

--- js/menu-tab.js
+++ js/menu-tab.js
@@ -1,15 +1,28 @@
+// 문서준비 event
 $(document).ready(function() {
-  var menu = $('.main-menu > li');
-  var subMenu = $('.sub-menu');
-  var last = $('.sub-menu li:last-child a');
+  var menu = $('main-menu > li');
 
   menu.hover(function() {
     $(this).find('.sub-menu').toggleClass('menu-act');
   });
   menu.focusin(function() {
+    $(this).siblings().find('.sub-menu').removeClass('menu-act');
     $(this).find('.sub-menu').addClass('menu-act');
   });
-  last.focusout(function() {
-    $(this).parents('.sub-menu').removeClass('menu-act');
-  });
 });
+
+// $(document).ready(function() {
+//   var menu = $('.main-menu > li');
+//   var subMenu = $('.sub-menu');
+//   var last = $('.sub-menu li:last-child a');
+
+//   menu.hover(function() {
+//     $(this).find('.sub-menu').toggleClass('menu-act');
+//   });
+//   menu.focusin(function() {
+//     $(this).find('.sub-menu').addClass('menu-act');
+//   });
+//   last.focusout(function() {
+//     $(this).parents('.sub-menu').removeClass('menu-act');
+//   });
+// });
